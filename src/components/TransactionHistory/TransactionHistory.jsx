c755e6ed50da92ec06d47cd909d2bfea1d0e216e
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-05-24 21:14:43 +0300

    Added TransactionHistory and transactions.json (3rd task)

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,30 @@
+import TransactionHistoryRow from "../TransactionHistoryRow/TransactionHistoryRow";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item) => {
+          return (
+            <TransactionHistoryRow
+              key={item.id}
+              type={item.type}
+              amount={item.amount}
+              currency={item.currency}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
